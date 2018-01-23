b2b0338c5348b57fb1d998f975c3514cb52dcca2
michellele994 <michellele@email.arizona.edu>
2018-01-23 03:02:00 -0700

    Made things look prettier(May add more when time allows). Need to organize and possibly add more functions for letters that have been guessed already.

--- hangman.js
+++ hangman.js
@@ -1,26 +1,27 @@
 var inquirer = require("inquirer");
 var request = require("request");
 var Word = require("./word.js");
-var pokemon = [];
-var lettersTried = [];
-var numOfTries = 10;
 var queryURL = "http://pokeapi.co/api/v2/pokemon/?limit=150";
 request(queryURL, function(error, response, body)
 {
 	if (!error)
 	{
+		var pokemon = [];
+		var lettersTried = [];
+		var numOfTries = 10;
+		var streak = 0;
+
 		bodyObject = JSON.parse(body);
 		for (var i = 0; i < 150; i++)
 		{
 			pokemon.push((bodyObject.results[i].name).toUpperCase());
 		}
+		pokemon[121] = "MRMIME";
 		var pokeToGuess = pokemon[Math.floor(Math.random()*pokemon.length)];
 		var wordToGuess = new Word(pokeToGuess);
-		cl("Let's play a game!")
+		cl("Do you want to be the very best?")
 		console.log(wordToGuess+" \n");
 		ask();
-
-
 
 		function ask() 
 		{
@@ -40,82 +41,87 @@ request(queryURL, function(error, response, body)
 					if (answer.guess === undefined || answer.guess.charAt(0) === " " || answer.guess.charAt(0) === "")
 					{
 						cl("You can't do that. You have to guess something.");
+						console.log(wordToGuess+" \n");
 						ask();
 					}
 					else if (answer.guess.length > 1)
 					{
 						cl("Too many at a time! Please enter only one character at a time.");
+						console.log(wordToGuess+" \n");
 						ask();
 					}
 					else if (answer.guess.charCodeAt(0) < 65 || (answer.guess.charCodeAt(0) > 90 && answer.guess.charCodeAt(0) < 97)  || answer.guess.charCodeAt(0) > 122)
 					{
 						cl("Don't be silly! Pick from the alphabet.");
+						console.log(wordToGuess+" \n");
 						ask();
 					}
 					else
 					{
 						answer.guess = answer.guess.toUpperCase();
 						answer.guess = answer.guess.charAt(0);
-						var hadTried = 0;
+						var alreadyTried = 0;
 						for (var i = 0; i < lettersTried.length; i++)
 						{
 							if (lettersTried[i] === answer.guess)
 							{
-								hadTried++;
+								alreadyTried++;
+							}
+						}
+						if (alreadyTried > 0)
+						{
+							cl("You already tried that letter, silly! Try another one.");
+							console.log(wordToGuess+" \n");
+							ask();
+							alreadyTried = 0;
+						}
+						else if (alreadyTried === 0)
+						{
+							lettersTried.push(answer.guess);
+							var numChanges = 0;
+							for (var i = 0; i < wordToGuess.wordArray.length; i++)
+							{
+								if (answer.guess === wordToGuess.wordArray[i].letter)
+								{
+									wordToGuess.wordArray[i].guess(answer.guess);
+									numChanges++;
+								}
+							}
+							if (numChanges === 0)
+							{
+								numOfTries--;
+								cl("Wrong! Number of tries left: " + numOfTries);
 							}
 							else
 							{
-								lettersTried.push(answer.guess);
-								console.log(lettersTried);
+								cl("CORRECT!")
+								numChanges = 0;
 							}
-						}
-						if (hadTried > 0)
-						{
-							cl("You already tried that letter, silly! Try another one.");
-							hadTried = 0;
-							ask();
-						}
-						var numFound  = 0;
-						for (var i = 0; i < wordToGuess.wordArray.length; i++)
-						{
-							if (answer.guess === wordToGuess.wordArray[i].letter)
-							{
-								wordToGuess.wordArray[i].guess(answer.guess);
-								numFound++;
-							}
-						}
-						if (numFound === 0)
-						{
-							numOfTries--;
-							console.log("\nWrong! Number of tries left: " + numOfTries + "\n");
-						}
-						else
-						{
-							console.log("\nCORRECT!\n")
-							numFound = 0;
-						}
-						// console.log(wordToGuess);
-						console.log(wordToGuess+" \n");
-
-
-						if (!wordToGuess.hasFound())
-						{
-							ask();
-						}
-						else if (wordToGuess.hasFound())
-						{
-							cl("YOU WON!!!! Keep going!! New pokemon!");
-							numOfTries = 10;
-							pokeToGuess = pokemon[Math.floor(Math.random()*pokemon.length)];
-							wordToGuess = new Word(pokeToGuess);
 							console.log(wordToGuess+" \n");
-							ask();
+
+
+							if (!wordToGuess.hasFound())
+							{
+								ask();
+							}
+							else if (wordToGuess.hasFound())
+							{
+								numOfTries = 10;
+								lettersTried = [];
+								pokeToGuess = pokemon[Math.floor(Math.random()*pokemon.length)];
+								wordToGuess = new Word(pokeToGuess);
+								streak++;
+								cl("YOU WON!!!! You put the 'COOL' in 'TENTACOOL'! Keep going!! Streak: " + streak);
+								console.log(wordToGuess+" \n");
+								ask();
+							}
 						}
 					}
 				}
 				else if (numOfTries <= 0)
 				{
-					console.log("You lost! The answer is: " + pokeToGuess + ". Keep studying.");
+					streak = 0;
+					console.log("You lost! You are as useful as a Magikarp. The answer is: " + pokeToGuess);
 				}
 			});
 		}
@@ -132,24 +138,3 @@ function cl (text)
 {
 	console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + text + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 }
-
-
-
-
-
-//prompt, are you ready to play?
-
-//start the game
-//pick a random word from the array and put it through the word.js
-
-
-// cat.wordArray[1].guess("a");
-// cat.wordArray[2].guess("s");
-// cat.wordArray[3].guess("o");
-// cat.wordArray[4].guess("n");
-
-// wordToGuess.hasFound();
-
-// console.log(wordToGuess + " ");
-// console.log(wordToGuess);
-
